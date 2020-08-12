22abfb0a89fc53734c369f893c473a49a0d5296d
EricksonLopes <ofc.erickson@gmail.com>
2020-08-12 09:32:44 -0300

    20. Sobrescrevendo os métodos genéricos

--- cursos/urls.py
+++ cursos/urls.py
@@ -2,8 +2,17 @@ from django.urls import path
 from .views import CursoAPIView, AvaliacaoAPIView, CursosAPIView, AvaliacoesAPIView
 
 urlpatterns = [
+    # Todos os cursos
     path('cursos/', CursosAPIView.as_view(), name='cursos'),
-    path('avaliacoes/', AvaliacaoAPIView.as_view(), name='avaliacoes'),
+    # Cada Curso em especifico
     path('cursos/<int:pk>/', CursoAPIView.as_view(), name='curso'),
-    path('avaliacoes/<int:pk>/', AvaliacoesAPIView.as_view(), name='avaliacao'),
+    # todas as avaliacoes de um curso
+    path('cursos/<int:curso_pk>/avaliacoes', AvaliacoesAPIView.as_view(), name='curso_avaliacoes'),
+    # avaliação especifica do curso
+    path('cursos/<int:curso_pk>/avaliacoes/<int:avaliacao_pk>/', AvaliacaoAPIView.as_view(), name='curso_avaliacao'),
+
+    # todas as avaliações
+    path('avaliacoes/', AvaliacaoAPIView.as_view(), name='avaliacoes'),
+    # Cada Avaliação em especifico
+    path('avaliacoes/<int:avaliacao_pk>/', AvaliacoesAPIView.as_view(), name='avaliacao'),
 ]
--- cursos/views.py
+++ cursos/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.generics import get_object_or_404
 
 from .models import Curso, Avalicacao
 from .serializers import CursoSerializers, AvaliacaoSerializer
@@ -18,7 +19,28 @@ class AvaliacoesAPIView(generics.ListCreateAPIView):
     queryset = Avalicacao.objects.all()
     serializer_class = AvaliacaoSerializer
 
+    def get_queryset(self):
+        # se algum parâmetro "curso_pk" for passado...
+        if self.kwargs.get('curso_pk'):
+            # Retorna o curso encontrado com o pk
+            return self.queryset.filter(curso_id=self.kwargs.get('curso_pk'))
+        # Se não, retorna todos os cursos
+        return self.queryset.all()
+
 
 class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Avalicacao.objects.all()
     serializer_class = AvaliacaoSerializer
+
+    def get_object(self):
+        # se algum parâmetro "curso_pk" for passado...
+        if self.kwargs.get('curso_pk'):
+            return get_object_or_404(self.get_queryset(),
+                                     # procura um curso pk com o parâmetro passado
+                                     curso_id=self.kwargs.get('curso_pk'),
+                                     # procura uma avaliazao pk com o parâmetro passado
+                                     pk=self.kwargs.get('avaliacao_pk'))
+
+        # caso contrario só procura um pk especificado
+        return get_object_or_404(self.get_queryset(), pk=self.kwargs.get('avaliacao_pk'))
+
