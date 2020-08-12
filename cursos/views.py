f9b8672f41cfa99e527f111e8caceac2855ad06c
EricksonLopes <ofc.erickson@gmail.com>
2020-08-12 06:38:38 -0300

    19. Criando um CRUD genérico com Django REST Framework

--- cursos/views.py
+++ cursos/views.py
@@ -1,60 +1,24 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics
+
 from .models import Curso, Avalicacao
 from .serializers import CursoSerializers, AvaliacaoSerializer
 
 
-class CursoAPIView(APIView):
-    """
-    API de cursos da Geek
-    """
-    # se a requisição for get
-    def get(self, request):
-        cursos = Curso.objects.all()
-        # many=True, quando for todos os dados / converte os items para json
-        serializer = CursoSerializers(cursos, many=True)
-        # retorna os itens convertidos como resposta
-
-        print(request.data)  # recebe dados
-        print(request.user)
-        print(request.query_params)
-
-        return Response(serializer.data)
-
-    def post(self, request):
-        # Pega od valores recebidos em json convertendo para o python ler
-        serializer = CursoSerializers(data=request.data)
-
-        # Verifica se os dados são validos
-        serializer.is_valid(raise_exception=True)
-
-        # Salva os dados
-        serializer.save()
-
-        # Retorna os dados recebidos com o status de 201
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        # Mensagem personalizada de retorno
-        # return Response({"msg": "Criou com sucesso"}, status=status.HTTP_201_CREATED)
-
-        # retornando somento o item especificado
-        # return Response({"id": serializer.data['id'], "titulo": serializer.data['titulo']},
-        #                 status=status.HTTP_201_CREATED)
+class CursosAPIView(generics.ListCreateAPIView):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializers
 
 
-class AvaliazaoAPIView(APIView):
-    """
-    API de avaliações da Geek
-    """
+class CursoAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializers
 
-    def get(self, request):
-        avaliacoes = Avalicacao.objects.all()
-        serializer = AvaliacaoSerializer(avaliacoes, many=True)
-        return Response(serializer.data)
 
-    def post(self, request):
-        serializer = AvaliacaoSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+class AvaliacoesAPIView(generics.ListCreateAPIView):
+    queryset = Avalicacao.objects.all()
+    serializer_class = AvaliacaoSerializer
+
+
+class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Avalicacao.objects.all()
+    serializer_class = AvaliacaoSerializer
